76f56efed65aabd1dfc6bb3f503e2d5b498d8e2c
acuza <adrian_cuza@yahoo.com>
2020-11-30 12:47:45 +0200

    Added custom dynamic jpa query methods.

--- src/main/java/com/springframework/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/com/springframework/spring5recipeapp/controllers/IndexController.java
@@ -1,14 +1,32 @@
 package com.springframework.spring5recipeapp.controllers;
 
+import com.springframework.spring5recipeapp.domain.Category;
+import com.springframework.spring5recipeapp.domain.UnitOfMeasure;
+import com.springframework.spring5recipeapp.repositories.CategoryRepository;
+import com.springframework.spring5recipeapp.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRespository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRespository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRespository = categoryRespository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
-        System.out.println("Some message to say... 1234");
+        Optional<Category> categoryOptional = categoryRespository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat Id is:" + categoryOptional.get().getId());
+        System.out.println("UOM Id is:" + unitOfMeasureOptional.get().getId());
         return "index";
     }
 }
