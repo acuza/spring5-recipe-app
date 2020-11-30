9bf11af4e837ab94899dbdc8abc4e5a1974811f8
acuza <adrian_cuza@yahoo.com>
2020-11-30 13:23:22 +0200

    Displayed list of recipes on the index page from the DB.

--- src/main/java/com/springframework/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/com/springframework/spring5recipeapp/controllers/IndexController.java
@@ -1,32 +1,22 @@
 package com.springframework.spring5recipeapp.controllers;
 
-import com.springframework.spring5recipeapp.domain.Category;
-import com.springframework.spring5recipeapp.domain.UnitOfMeasure;
-import com.springframework.spring5recipeapp.repositories.CategoryRepository;
-import com.springframework.spring5recipeapp.repositories.UnitOfMeasureRepository;
+import com.springframework.spring5recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 @Controller
 public class IndexController {
 
-    private CategoryRepository categoryRespository;
-    private UnitOfMeasureRepository unitOfMeasureRepository;
+    private final RecipeService recipeService;
 
-    public IndexController(CategoryRepository categoryRespository, UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categoryRespository = categoryRespository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
     @RequestMapping({"", "/", "/index"})
-    public String getIndexPage() {
-        Optional<Category> categoryOptional = categoryRespository.findByDescription("American");
-        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
-
-        System.out.println("Cat Id is:" + categoryOptional.get().getId());
-        System.out.println("UOM Id is:" + unitOfMeasureOptional.get().getId());
+    public String getIndexPage(Model model) {
+        model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
 }
